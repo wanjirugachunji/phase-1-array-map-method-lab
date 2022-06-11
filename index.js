ca04659b7bd6a763faea4367da58abf4edad543a
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-11 16:10:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
 ];
 
 const titleCased = () => {
   return tutorials
+  return tutorials.map((tutorial)=>{
+    const temps = tutorial.split(' ');
+    const toMerge = temps.map((word)=>{
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    return toMerge.join(' ')  
+  })
 }
+
